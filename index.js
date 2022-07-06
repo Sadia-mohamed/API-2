3d0e479003c2edb03fd927ead75cecdc2c2c7f1b
sadia-mohamed <sacduushm@gmail.com>
2022-07-06 16:54:37 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+let countryName = document.getElementById("country-name");
+let countryFlag = document.getElementById("country-flag");
+let searchBtn = document.getElementById("search");
+
+searchBtn.addEventListener("submit" ,(e) =>{
+    
+    e.preventDefault();
+    
+fetch(`https://restcountries.com/v3.1/name/${e.target[0].value}`)
+.then((res) => res.json())
+.then((data) =>{
+    
+    countryName.textContent=data[0].name.common;
+    countryFlag.innerHTML = `<img src = ${data[0].flags.png} alt ="country flag" >`;
+});
+});
+
+
+
+
